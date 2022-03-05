1da430688ff1d8c998f7c56a38e29d020220ab66
kipriox <kipriox@mail.ru>
2022-03-05 16:17:39 +0300

    Добавление графиков

--- Chartexamples.py
+++ Chartexamples.py
@@ -1,6 +1,8 @@
 #Примеры построения графиков
 
 import tkinter as tk
+import Chart1
+import Chart2
 
 #Функция закрытия программы
 def do_close():
@@ -12,8 +14,22 @@ window.geometry("450x450")
 window.title("Примеры построения графиков")
 
 #Добавление метки заголовка
-lblTitle = tk.Label (text = "Примеры построения графиков", font = ('Helvetica', 10, 'bold'), fg = '#0000cc')
+lblTitle = tk.Label (text = "Примеры построения графиков", font = ('Helvetica', 16, 'bold'), fg = '#0000cc')
 lblTitle.place(x=55, y=25)
+
+#Добавление кнопки и метки для графика 1
+btnChart1 = tk.Button(window, text="График 1", font = ('Helvetica', 10, 'bold'), command=Chart1.plot_chart)
+btnChart1.place(x=40, y=115, width=90, height=30)
+
+lblChart1 = tk.Label(text = "График синуса matplotlib")
+lblChart1.place(x=170, y=122)
+
+#Добавление кнопки и метки для графика 2
+btnChart2 = tk.Button(window, text="График 2", font = ('Helvetica', 10, 'bold'), command=Chart2.plot_chart)
+btnChart2.place(x=40, y=165, width=90, height=30)
+
+lblChart2 = tk.Label(text = "Нормальное распределение")
+lblChart2.place(x=170, y=172)
 
 #Добавление кнопки закрытия программы
 btnClose = tk.Button(window,text='Закрыть', font = ('Helvetica', 10, 'bold'), command=do_close)
